01fdd62959b465408d3bd0ff81f4b112724fc212
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-02-12 19:19:37 +0530

    Add files via upload

--- gates2020/settings.py
+++ gates2020/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
+import psycopg2
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -118,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+)
+
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static', 'root')
+django_heroku.settings(locals())
