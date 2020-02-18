e06309f7149e53977f7f01b4d2b49679a2ff89c3
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-02-19 04:01:42 +0530

    Add files via upload

--- static/js/redirectUrl.js
+++ static/js/redirectUrl.js
@@ -1,14 +1,28 @@
 const redirectUrlText = (text,redirect) => {
+    document.getElementById("redirectUrlText").innerHTML = "";
     document.getElementById("redirect-url-overlay").classList.remove("none");
     var textDisplayArr = text.split("");
-    var textDisplay = "";
+    var RedirectTextDelay = 1.5;
+    var RedirectGPdelay = 0.05;
     for (var i = 0;i<textDisplayArr.length;i++){
-        var intermediateText = `<div class="redirectText">${textDisplayArr[i]}</div>`;
-        textDisplay+=intermediateText;
+        var newElmt = document.createElement("div");
+        newElmt.className = "redirectText";
+        newElmt.style.animationDelay = `${RedirectTextDelay + i * RedirectGPdelay}s`;
+        newElmt.innerText = textDisplayArr[i];
+        document.getElementById("redirectUrlText").appendChild(newElmt);
     }
-    console.log(textDisplay);
-    document.getElementById("redirectUrlText").innerHTML = textDisplay;
-    setTimeout(() => {
-        window.location = `/${redirect}`;
-    }, 3000);
+    if (redirect != ""){
+        setTimeout(() => {
+            window.location = `/${redirect}`;
+        }, 3000);
+    }
+    else{
+        setTimeout(() => {
+            document.getElementById("redirect-url-overlay").classList.add("ScaleDownAll");
+        }, 3500);
+        setTimeout(()=>{
+            document.getElementById("redirect-url-overlay").classList.add("none");
+            document.getElementById("redirect-url-overlay").classList.remove("ScaleDownAll");
+        },5000);
+    }
 }
