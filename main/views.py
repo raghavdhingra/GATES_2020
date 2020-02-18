ac9d4f4a4a8b90f491032585a695e0cea027d8f0
Raghav Dhingra <raghav.dhingra15@gmail.com>
2020-02-18 16:15:02 +0530

    Add files via upload

--- main/views.py
+++ main/views.py
@@ -6,3 +6,21 @@ def home(request):
         "title": "GATES 2020 | GTBIT Official Techno-Cultural Fest Event for creative Souls",
     }
     return render(request,"index.html",context)
+
+def about(request):
+    context = {
+
+    }
+    return render(request,"about.html",context)
+
+def developer(request):
+    context = {
+
+    }
+    return render(request,"developer.html",context)
+
+def starNight(request):
+    context = {
+
+    }
+    return render(request,"starNight.html",context)
